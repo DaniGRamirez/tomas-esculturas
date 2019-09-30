472ada80e07d1fb2da52cb4465449ff5b7235b75
DANIEL <daniel@iMac-de-DANIEL.local>
2019-09-30 18:29:44 +0200

    commit test mail

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const port = process.env.PORT || 8080;
 const app = express();
 const nodemailer = require('nodemailer');
 
-
+console.log("test mail");
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 //let testAccount = await nodemailer.createTestAccount();
 
+document.getElementById("testEmail").addEventListener("onclick",function(){
+  console.log("Prueba mail");
+    alert("Prueba de mail correcta");
+});
+
 var smtpTransport = nodemailer.createTransport({
-    host: 'gmail',
+    service: 'gmail',
     secure: true,
     auth: {           
         user: 'tomasgr.escultura@gmail.com',
@@ -19,9 +24,9 @@ var smtpTransport = nodemailer.createTransport({
 });
 
 var mailOptions = {
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Dani 👻" <danigramirez27@gmail.com>', // sender address
     to: 'tomasgr.escultura@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
+    subject: 'Prueba email dani ✔', // Subject line
     text: 'Hello world?', // plain text body
     html: '<b>Hello world?</b>' // html body
   };
--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,42 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './Contacto.css';
-const nodemailer = require('nodemailer');
-
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//     // Generate test SMTP service account from ethereal.email
-//     // Only needed if you don't have a real mail account for testing
-//     //let testAccount = await nodemailer.createTestAccount();
-   
-//     var smtpTransport = nodemailer.createTransport({
-//         host: 'gmail',
-//         secure: true,
-//         auth: {           
-//             user: 'tomasgr.escultura@gmail.com',
-//             pass: 'hornocaido'
-//         }
-//     });
-
-//     var mailOptions = {
-//         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//         to: 'tomasgr.escultura@gmail.com', // list of receivers
-//         subject: 'Hello ✔', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: '<b>Hello world?</b>' // html body
-//       };
-           
-
-//       smtpTransport.sendMail(mailOptions, function(error){
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.log('Email sent');
-//         }       
-//       });          
-// }
-
-// main().catch(console.error);
 
 class Contacto extends Component {              
 
@@ -50,7 +14,10 @@ class Contacto extends Component {
                 <div className = "ContactElement">
                     <img src=" https://image.flaticon.com/icons/svg/0/191.svg"></img>   
                     <h1>+34-609278080</h1>                                 
-                </div>     
+                </div>   
+                <button id="testEmail">
+                    Test
+                </button>  
         </div> 
     )}
 
