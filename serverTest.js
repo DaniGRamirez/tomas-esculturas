1a13af5ddd9d4807bcd11fadc01e143f4bf850fe
DANIEL <daniel@iMac-de-DANIEL.local>
2019-10-02 12:08:38 +0200

    commit heroku

--- serverTest.js
+++ serverTest.js
@@ -2,12 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send("Hello");
-})
-
-app.get('/ping', function (req, res) {
-    return res.send('pong');
+app.get('/test', function (req, res) {
+    return res.send(' response to test');
    });
 
 app.listen(3000);
