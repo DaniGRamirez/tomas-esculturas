72bcc6a92c4e399f6b3a43c3595bcd3b37e519df
DANIEL <daniel@iMac-de-DANIEL.local>
2019-10-04 12:37:45 +0200

    style form done

--- server.js
+++ server.js
@@ -5,59 +5,6 @@ const app = express();
 const nodemailer = require('nodemailer');
 var bodyParser = require('body-parser')
 
-
-const oauth2Client = new OAuth2(
-  '825503344034-ejv83of9mmmihot31f1b1r4b1r4gtnmb.apps.googleusercontent.com', // ClientID
-  'zkGWBMYJ4xUCE_RQoDTEeVW4', // Client Secret
-  // "https://developers.google.com/oauthplayground" // Redirect URL
-);
-
-oauth2Client.setCredentials({
-  refresh_token: "1/vVASjgmg9vn7Cm4ZrLvUMXD__jaorFPD5IDknrx3sqw"
-});
-
-console.log(oauth2Client);
-const accessToken = oauth2Client.getAccessToken();
-
-// console.log(accessToken);
-
-let transporterToken = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-      type: 'OAuth2',
-      user: 'tomasgr.escultura@gmail.com',
-      clientId: '825503344034-ejv83of9mmmihot31f1b1r4b1r4gtnmb.apps.googleusercontent.com',
-      clientSecret: 'zkGWBMYJ4xUCE_RQoDTEeVW4',
-      refreshToken: '1/vVASjgmg9vn7Cm4ZrLvUMXD__jaorFPD5IDknrx3sqw',
-      // accessToken: 'ya29.Il-UB1thoateU9n-x4DFmJ0lD2JY4p1tR56zorcNlWhckkyXcyCvlwzkLGRjX-kr4eEk2HISyTpC2oMyt9B6cvB6M39yZaDZt3TTGfNVyNi6yPo73VzSK7RtrZYy-iFnlA',
-      // expires: 1484314697598
-  }
-});
-
-var mailOptionsToken = {
-  from: `dani`, // sender address
-  to: 'tomasgr.escultura@gmail.com', // list of receivers
-  subject: `Formulario web - Test token`, // Subject line       
-  html: ` mensaje enviado desde:`, // plain text body       
-  replyTo:`NONE`,
-};      
-
-
-transporterToken.sendMail(mailOptionsToken,function(error){
-  console.log("Email acces-token");
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent');
-      res.send("ok");
-    }       
-  }  
-)
-
-
-
 console.log("test mail server side");
 
 var smtpTransport = nodemailer.createTransport({
